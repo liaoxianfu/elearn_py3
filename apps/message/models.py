8b24abe0280732162ae65758cbbac4b1bed703a4
liaoxianfu <liaoxianfu555@outlook.com>
2018-03-15 10:37:17 +0800

    Initial commit

--- apps/message/models.py
+++ apps/message/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class UserProfile(AbstractUser):
+    nick_name=models.CharField(
+        max_length=50,
+        verbose_name=u'昵称',
+        default='',
+        birthday=models.DateTimeField(
+            verbose_name=u'生日'
+        )
+    )
+
